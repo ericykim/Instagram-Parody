d21bbaefb19e5c7997fd1728144ea0b228041579
ericykim <ericykim5399@gmail.com>
2016-06-27 14:20:42 -0700

    First Commit

---
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Benjamin Encz on 5/15/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+
+import UIKit
+import Parse
+import ConvenienceKit
+
+class TimelineViewController: UIViewController, TimelineComponentTarget {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let defaultRange = 0...4
+    let additionalRangeSize = 5
+    
+    var timelineComponent: TimelineComponent<Post, TimelineViewController>!
+    var posts: [Post] = []
+    var photoTakingHelper: PhotoTakingHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timelineComponent = TimelineComponent(target: self)
+        self.tabBarController?.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        timelineComponent.loadInitialIfRequired()
+    }
+    
+    func loadInRange(range: Range<Int>, completionBlock: ([Post]?) -> Void) {
+        // 1
+        ParseHelper.timelineRequestForCurrentUser(range) { (result: [PFObject]?, error: NSError?) -> Void in
+            // 2
+            let posts = result as? [Post] ?? []
+            // 3
+            completionBlock(posts)
+        }
+    }
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            let post = Post()
+            // 1
+            post.image.value = image!
+            post.uploadPost()
+        }
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timelineComponent.content.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        
+        let post = timelineComponent.content[indexPath.row]
+        post.downloadImage()
+        post.fetchLikes()
+        cell.post = post
+        
+        return cell
+    }
+    
+}
+
+extension TimelineViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        timelineComponent.targetWillDisplayEntry(indexPath.row)
+    }
+    
+}
